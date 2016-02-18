da785e4f953fd56f3d19dcf39119d1e3b65fac1a
Ron <mail@r0n.nl>
2016-02-18 14:20:35 +0100

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Remote Service Display module for ZPanelX (uses XMWS)
+ * Remote Service Display module for ZPanelX and Sentora (uses XMWS)
  * Written by Bobby Allen, 05/04/2012. 
  * 
  * You need to modify these to match your server set-up!
  * If you don't know what your API key is, grab the 'gatekeeper' module to manage your server's API key easier!
+ * *You can find GateKeeper here: https://github.com/auxio/GateKeeper
  */
 
-// Your ZPanel Server's API Key (eg. "c44558c3j23cd510e2908241ea9985f8393b52d9")
+// Your ZPanel/Sentora Server's API Key (eg. "c44558c3j23cd510e2908241ea9985f8393b52d9")
 $apikey = (string) "";
 
-// Your ZPanel Server's URL (eg. "http://zpanel.yourdomain.com")
+// Your ZPanel/Sentora Server's URL (eg. "http://zpanel.yourdomain.com")
 $apiurl = (string) "";
 ?>
